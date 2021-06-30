afbcee7e075a4df97e0bda3e311585c35de25a51
tdevere <tdevere@microsoft.com>
2021-06-29 19:19:30 -0500

    module approach

---
+++ ProjectRoot/module_one/src/main/java/com/example/module_one/ModuleOne_MainActivity.kt
@@ -0,0 +1,11 @@
+package com.example.module_one
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ModuleOne_MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_module_one__main)
+    }
+}
---
+++ ProjectRoot/module_two/src/main/java/com/example/module_two/module_two_MainActivity.kt
@@ -0,0 +1,11 @@
+package com.example.module_two
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class module_two_MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_module_two__main)
+    }
+}
